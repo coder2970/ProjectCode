6da743f17db223a88f5581710836edfb8a1ecf58
coder2970 <13955275327@163.com>
2026-03-09 22:21:33 +0800

    util模块更新

--- OnlineJudge/Common/util.hpp
+++ OnlineJudge/Common/util.hpp
@@ -1,14 +1,36 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <atomic>
+#include <fstream>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <boost/algorithm/string.hpp>
 
 // 工具类
 namespace ns_util
 {
+    // 时间功能
+    class TimeUtil
+    {
+    public:
+        // 获取系统时间戳 - 使用gettimeofday接口
+        static std::string GetTimeStamp()
+        {
+            timeval _time;
+            gettimeofday(&_time, nullptr);
+            return std::to_string(_time.tv_sec); // 获得秒数
+        }
+        // 获得毫秒级时间戳
+        static std::string GetTimeMs()
+        {
+            timeval _time;
+            gettimeofday(&_time, nullptr);
+            return std::to_string(_time.tv_sec * 1000 + _time.tv_usec / 1000);
+        }
+    };
     const std::string temp_path = "./temp/";
     // 路径拼接类:对路径进行操作
     class PathUtil
@@ -73,30 +95,56 @@ namespace ns_util
         // 形成唯一文件名
         static std::string UniqueFileName()
         {
-            return "";
+            static std::atomic_uint id(0);
+            id++;
+            std::string ms = TimeUtil::GetTimeMs();
+            std::string unique_id = std::to_string(id);
+            return ms + "_" + unique_id;
         }
 
         // 向文件写入数据
-        static bool WriteFile(const std::string& target, const std::string& code)
+        static bool WriteFile(const std::string &target, const std::string &content)
         {
-
+            std::ofstream out(target);
+            if (!out.is_open())
+            {
+                return false;
+            }
+            out.write(content.c_str(), content.size());
+            out.close();
+            return true;
         }
 
         // 读取文件
-        static std::string& ReadFile(const std::string& target)
-        {}
-    };
-
-    // 时间功能
-    class TimeUtil
-    {
-    public:
-        // 获取系统时间戳 - 使用gettimeofday接口
-        static std::string GetTimeStamp()
+        static bool ReadFile(const std::string &target, std::string *content, bool keep = false)
         {
-            timeval _time;
-            gettimeofday(&_time, nullptr);
-            return std::to_string(_time.tv_sec); // 获得秒数
+            (*content).clear();
+            std::ifstream in(target);
+            if (!in.is_open())
+            {
+                return false;
+            }
+            std::string line;
+            // getline 不保存行分隔符\n
+            // getline内重载了强制类型转换
+            while (std::getline(in, line))
+            {
+                (*content) += line;
+                (*content) += keep ? "\n" : "";
+            }
+            in.close();
+            return true;
+        }
+    };
+    class StringUtil
+    {
+        public:
+        // str 目标切割字符串
+        // 切割字符串 , 切割后吗一个字符串 -> 多个字符串, vector存储(输出参数)
+        // sep 指定分隔符
+        static void SplitString(const std::string& str, std::vector<std::string> *target, const std::string& sep)
+        {
+            boost::split(*target, str, boost::is_any_of(sep), boost::algorithm::token_compress_on);
         }
     };
 }
--- OnlineJudge/test.cc
+++ OnlineJudge/test.cc
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <vector>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <ctemplate/template.h>
+#include <boost/algorithm/string.hpp>
 
 int main()
 {
@@ -9,15 +12,41 @@ int main()
     // r.rlim_max = RLIM_INFINITY;
     // setrlimit(RLIMIT_CPU, &r);
 
-    struct rlimit r;
-    r.rlim_cur = 1024 * 1024 * 20;
-    r.rlim_max = RLIM_INFINITY;
-    setrlimit(RLIMIT_AS, &r);
-    int count = 1;
-    while (1)
-    {
-        int *p = new int[1024 * 1024];
-        std::cout << count++ << std::endl;
-    }
+    // struct rlimit r;
+    // r.rlim_cur = 1024 * 1024 * 20;
+    // r.rlim_max = RLIM_INFINITY;
+    // setrlimit(RLIMIT_AS, &r);
+    // int count = 1;
+    // while (1)
+    // {
+    //     int *p = new int[1024 * 1024];
+    //     std::cout << count++ << std::endl;
+    // }
+
+    // ------------------------------------------------------------
+    // 使用boost库进行字符串切分
+    // const std::string str = "1:判断回文:简单:1::::30000";
+    // std::vector<std::string> tokens;
+    // boost::split(tokens, str, boost::is_any_of(":"), boost::algorithm::token_compress_on);
+    // for(auto iter : tokens)
+    //     std::cout << iter << std::endl;
+
+    // 使用ctemplate进行网页渲染
+    // ------------------------------------------------------------
+    std::string in_html = "./test.html";
+    std::string value = "tangjunyao";
+
+    // 形成数据字典
+    ctemplate::TemplateDictionary root("test"); // 类似于unordered_map<> test
+    root.SetValue("key", value);                // test.insert({});
+
+    // 获取被渲染网页对象
+    ctemplate::Template *tpl = ctemplate::Template::GetTemplate(in_html, ctemplate::DO_NOT_STRIP); // 保持网页原貌
+
+    // 添加字典数据到网页
+    std::string out_html;
+    tpl->Expand(&out_html, &root);
+    // 完成渲染
+    std::cout << out_html << std::endl;
     return 0;
 }
